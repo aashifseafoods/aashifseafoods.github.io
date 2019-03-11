4dcb9cf0ee3f53136f158324ec5b8fc965e51d12
aashifseafoods <48433511+aashifseafoods@users.noreply.github.com>
2019-03-11 15:20:37 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+if (screen.width <= 576){
+    
+    var element = document.getElementsByClassName('tab-pane');
+    
+    element[1].classList.add("show");
+    element[1].classList.add("active");
+
+    element[2].classList.add("show");
+    element[2].classList.add("active");
+}
